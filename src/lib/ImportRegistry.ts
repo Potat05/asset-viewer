bdc6647b8002a7d38635866fad85df19cfd9aa64
Potat05 <williamkoerber6@gmail.com>
2023-09-11 17:36:05 -0500

    improve ImportRegistry usability

--- src/lib/ImportRegistry.ts
+++ src/lib/ImportRegistry.ts
@@ -17,17 +17,23 @@ export class ImportRegistry<ValidateType, ImportType, ExtraData = void> {
     
     public items: RegistryItem<ValidateType, ImportType, ExtraData, false>[] = [];
 
+    private async itemImport(item: RegistryItem<ValidateType, ImportType, ExtraData, false>): Promise<RegistryItem<ValidateType, ImportType, ExtraData, true>> {
+
+        if(item.import === null) {
+            item.import = (await item.importfn()).default;
+        }
+
+        return item as unknown as RegistryItem<ValidateType, ImportType, ExtraData, true>;
+
+    }
+
     public async get(value: ValidateType): Promise<RegistryItem<ValidateType, ImportType, ExtraData, true> | null> {
 
         for(const item of this.items) {
 
             if(await item.isValid(value)) {
 
-                if(item.import == null) {
-                    item.import = (await item.importfn()).default;
-                }
-
-                return item as unknown as RegistryItem<ValidateType, ImportType, ExtraData, true>;
+                return await this.itemImport(item);
 
             }
 
@@ -39,6 +45,28 @@ export class ImportRegistry<ValidateType, ImportType, ExtraData = void> {
 
     public async getImport(value: ValidateType): Promise<ImportType | null> {
         return (await this.get(value))?.import ?? null;
+    }
+
+    public async getAll(value: ValidateType): Promise<RegistryItem<ValidateType, ImportType, ExtraData, true>[]> {
+
+        let items = [];
+
+        for(const item of this.items) {
+
+            if(await item.isValid(value)) {
+
+                items.push(await this.itemImport(item));
+
+            }
+
+        }
+
+        return items;
+
+    }
+
+    public async getAllImports(value: ValidateType): Promise<ImportType[]> {
+        return (await this.getAll(value)).map(item => item.import);
     }
 
     public async valid(value: ValidateType): Promise<boolean> {
