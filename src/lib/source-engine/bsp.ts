2a4fdce676d001a1adadb39082fa25bb054a33f3
Potat05 <williamkoerber6@gmail.com>
2023-09-16 14:28:59 -0500

    bsp viewer points

--- src/lib/source-engine/bsp.ts
+++ src/lib/source-engine/bsp.ts
@@ -17,14 +17,14 @@ function decompressLZMA(compressedData: ArrayBuffer, uncompressedSize: number): 
         throw new Error('BSP decompressing lzma size does not match.');
     }
     const lzmaSize = compressedView.getUint32(0x08, true);
-    if(lzmaSize + 0x11 <= compressedData.byteLength) {
+    if(lzmaSize + 0x11 > compressedData.byteLength) {
         throw new Error('BSP decompressing lzma size does not match.');
     }
 
     const lzmaProperties = decodeLZMAProperties(compressedData.slice(0x0C));
     const uncompressed = decompress(compressedData.slice(0x11), lzmaProperties, actualSize);
 
-    if(actualSize == uncompressed.byteLength) {
+    if(actualSize != uncompressed.byteLength) {
         throw new Error('BSP decompressing lzma size does not match.');
     }
 
@@ -40,6 +40,84 @@ type LumpDirEntry = {
     length: number;
     version: number;
     uncompressedLength: number;
+}
+
+enum Lump {
+    ENTITIES = 0,
+    PLANES = 1,
+    TEXDATA = 2,
+    VERTEXES = 3,
+    VISIBILITY = 4,
+    NODES = 5,
+    TEXINFO = 6,
+    FACES = 7,
+    LIGHTING = 8,
+    OCCLUSION = 9,
+    LEAFS = 10,
+    FACEIDS = 11,
+    EDGES = 12,
+    SURFEDGES = 13,
+    MODELS = 14,
+    WORLDLIGHTS = 15,
+    LEAFFACES = 16,
+    LEAFBRUSHES = 17,
+    BRUSHES = 18,
+    BRUSHSIDES = 19,
+    AREAS = 20,
+    AREAPORTALS = 21,
+    PORTALS = 22,
+    UNUSED0 = 22,
+    PROPCOLLISION = 22,
+    CLUSTERS = 23,
+    UNUSED1 = 23,
+    PROPHULLS = 23,
+    PORTALVERTS = 24,
+    UNUSED2 = 24,
+    PROPHULLVERTS = 24,
+    CLUSTERPORTALS = 25,
+    UNUSED3 = 25,
+    PROPTRIS = 25,
+    DISPINFO = 26,
+    ORIGINALFACES = 27,
+    PHYSDISP = 28,
+    PHYSCOLLIDE = 29,
+    VERTNORMALS = 30,
+    VERTNORMALINDICES = 31,
+    DISP_LIGHTMAP_ALPHAS = 32,
+    DISP_VERTS = 33,
+    DISP_LIGHTMAP_SAMPLE_POSITIONS = 34,
+    GAME_LUMP = 35,
+    LEAFWATERDATA = 36,
+    PRIMITIVES = 37,
+    PRIMVERTS = 38,
+    PRIMINDICES = 39,
+    PAKFILE = 40,
+    CLIPPORTALVERTS = 41,
+    CUBEMAPS = 42,
+    TEXDATA_STRING_DATA = 43,
+    TEXDATA_STRING_TABLE = 44,
+    OVERLAYS = 45,
+    LEAFMINDISTTOWATER = 46,
+    FACE_MACRO_TEXTURE_INFO = 47,
+    DISP_TRIS = 48,
+    PHYSCOLLIDESURFACE = 49,
+    PROP_BLOB = 49,
+    WATEROVERLAYS = 50,
+    LIGHTMAPPAGES = 51,
+    LEAF_AMBIENT_INDEX_HDR = 51,
+    LIGHTMAPPAGEINFOS = 52,
+    LEAF_AMBIENT_INDEX = 52,
+    LIGHTING_HDR = 53,
+    WORLDLIGHTS_HDR = 54,
+    LEAF_AMBIENT_LIGHTING_HDR = 55,
+    LEAF_AMBIENT_LIGHTING = 56,
+    XZIPPAKFILE = 57,
+    FACES_HDR = 58,
+    MAP_FLAGS = 59,
+    OVERLAY_FADES = 60,
+    OVERLAY_SYSTEM_LEVELS = 61,
+    PHYSLEVEL = 62,
+    DISP_MULTIBEND = 63
 }
 
 
@@ -83,16 +161,22 @@ export class BSP extends BlobReader {
 
     }
 
-    public async getRawLumpBuffer(index: number): Promise<ArrayBuffer | null> {
+    public lumpExists(index: Lump): boolean {
+        if(index < 0 || index >= 64) return false;
+        const lump = this.lumpDirEntries[index];
+        if(lump.length != 0) return true;
+        if(lump.offset != 0) return true;
+        return false;
+    }
+
+    public async getRawLumpBuffer(index: Lump): Promise<ArrayBuffer | null> {
+        if(!this.lumpExists(index)) return null;
 
         const entry = this.lumpDirEntries[index];
-        if(!entry) return null;
-
-        if(entry.length == 0 || entry.offset == 0) return null;
 
         const buffer = await this.blob.slice(entry.offset, entry.offset + entry.length).arrayBuffer();
 
-        if(entry.uncompressedLength == 0) {
+        if(entry.uncompressedLength === 0) {
             return buffer;
         }
 
@@ -100,7 +184,7 @@ export class BSP extends BlobReader {
 
     }
 
-    public async getRawLumpBlob(index: number): Promise<Blob | null> {
+    public async getRawLumpBlob(index: Lump): Promise<Blob | null> {
 
         const entry = this.lumpDirEntries[index];
         if(!entry) return null;
@@ -117,9 +201,14 @@ export class BSP extends BlobReader {
 
     }
 
+    public async loadLump(index: Lump): Promise<void> {
+        const buffer = await this.getRawLumpBuffer(index);
+        this.loadData(buffer ?? new ArrayBuffer(0));
+    }
+
     public async getPakFile(name: string, parent: fsDirectory | null): Promise<fsDirectory | null> {
 
-        const archiveBlob = await this.getRawLumpBlob(40); // PAKFILE = 40
+        const archiveBlob = await this.getRawLumpBlob(Lump.PAKFILE);
 
         if(archiveBlob == null) return null;
 
@@ -127,6 +216,17 @@ export class BSP extends BlobReader {
 
         return zip;
 
+    }
+
+    public async getVertices() {
+        await this.loadLump(Lump.VERTEXES);
+        return this.readArrayUntilEnd(() => {
+            return {
+                x: this.readNumber('Float32'),
+                y: this.readNumber('Float32'),
+                z: this.readNumber('Float32')
+            }
+        });
     }
 
 }
--- src/lib/viewers/source-engine/bsp.ts
+++ src/lib/viewers/source-engine/bsp.ts
@@ -1,19 +1,129 @@
-import { fsEntry } from "$lib/FileSystem";
+import { fsEntry, type fsDirectory, type fsFile } from "$lib/FileSystem";
 import type { Viewer } from "$lib/Viewer";
 import { BSP } from "$lib/source-engine/bsp";
+import * as THREE from "three";
+import { PointerLockControls } from "three/examples/jsm/controls/PointerLockControls";
+
+
+
+// TODO: This is sorta jank, maybe do this differently?
+type StoredTransformFsEntry = (fsFile | fsDirectory) & { old?: (fsFile | fsDirectory) };
+
+
 
 const viewer: Viewer = {
     namespace: 'source-engine.bsp',
     priority: 2,
-    // createViewer: async (entry, target) => {
+    createViewer: async (entry: StoredTransformFsEntry, target) => {
 
-    //     if(entry.type == fsEntry.File) {
+        if(entry.type == fsEntry.Directory) {
 
-    //     } else {
-    //         throw new Error('Tried to create bsp viewer with directory.');
-    //     }
+            const file = entry.old;
+            
+            if(!file || file.type == fsEntry.Directory) {
+                throw new Error('Tried to create bsp viewer with transformed viewer that has no file.');
+            }
 
-    // },
+
+
+            const bsp = new BSP(await file.blob());
+            await bsp.readHeader();
+
+
+
+            // Init renderer
+            const canvas = document.createElement('canvas');
+            canvas.style.width = '100vw';
+            canvas.style.height = '100vh';
+            const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
+            const scene = new THREE.Scene();
+            const camera = new THREE.PerspectiveCamera(91, 1 / 1, 1, 20000);
+            const controls = new PointerLockControls(camera, canvas);
+
+            camera.position.set(0, 100, 0)
+
+
+
+            let keys: Set<string> = new Set();
+
+            function updateControls(dt: number = 1) {
+                let move = new THREE.Vector3();
+                let speed = 1;
+
+                const forward = camera.getWorldDirection(new THREE.Vector3());
+                const up = camera.up;
+                const right = new THREE.Vector3().crossVectors(forward, up);
+
+                if(keys.has('KeyW')) move.add(forward);
+                if(keys.has('KeyS')) move.addScaledVector(forward, -1);
+                if(keys.has('KeyD')) move.add(right);
+                if(keys.has('KeyA')) move.addScaledVector(right, -1);
+                if(keys.has('Space')) move.add(up);
+                if(keys.has('ControlLeft')) move.addScaledVector(up, -1);
+
+                camera.position.addScaledVector(move.normalize(), speed * dt);
+            }
+
+            function keyDown(ev: KeyboardEvent) {
+                keys.add(ev.code);
+            }
+        
+            function keyUp(ev: KeyboardEvent) {
+                keys.delete(ev.code);
+            }
+
+            document.body.addEventListener('keydown', keyDown);
+            document.body.addEventListener('keyup', keyUp);
+
+            canvas.addEventListener('click', () => controls.lock());
+
+
+
+            new ResizeObserver(() => {
+                canvas.width = canvas.clientWidth;
+                canvas.height = canvas.clientHeight;
+                renderer.setSize(canvas.width, canvas.height);
+                camera.aspect = canvas.width / canvas.height;
+                camera.updateProjectionMatrix();
+            }).observe(canvas);
+
+
+
+            // Create scene
+            const vertices = await bsp.getVertices();
+
+            const geom = new THREE.BufferGeometry();
+            geom.setAttribute('position', new THREE.Float32BufferAttribute(vertices.map(vert => [ vert.x, vert.y, vert.z ]).flat(), 3));
+
+            const mesh = new THREE.Points(geom, new THREE.PointsMaterial({
+                size: 5,
+                color: 'white'
+            }));
+
+            mesh.rotateX(Math.PI / 2);
+
+            scene.add(mesh);
+
+
+
+            // Render loop
+            setInterval(() => {
+
+                if(controls.isLocked) {
+                    updateControls(1000 / 60);
+                }
+
+                renderer.render(scene, camera);
+
+            }, 1000 / 60);
+
+            target.appendChild(canvas);
+
+        } else {
+            throw new Error('Tried to create bsp viewer with file.');
+        }
+
+    },
     transform: async entry => {
         
         if(entry.type != fsEntry.File) {
@@ -21,10 +131,16 @@ const viewer: Viewer = {
         }
 
         const bsp = new BSP(await entry.blob());
-
         await bsp.readHeader();
 
-        return await bsp.getPakFile(entry.name, entry.parent);
+        const pakfile = await bsp.getPakFile(entry.name, entry.parent);
+
+        if(!pakfile) return null;
+
+        const dir: StoredTransformFsEntry = pakfile;
+        dir.old = entry;
+
+        return dir;
 
     },
     getIcon: async () => {
