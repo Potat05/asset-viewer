de45b300ae760f7f51ec1ac969e7c19b66011954
Potat05 <williamkoerber6@gmail.com>
2023-09-30 14:12:44 -0500

    better bsp overlays

--- src/lib/source-engine/bspBuilder.ts
+++ src/lib/source-engine/bspBuilder.ts
@@ -2,6 +2,7 @@ import { fsUtils, type fsDirectory, fsEntry } from "$lib/FileSystem";
 import type { BSPReader, Lump } from "./bspReader";
 import * as THREE from "three";
 import { VMT } from "./vmt";
+import { ThreeUtils } from "$lib/ThreeUtils";
 
 
 
@@ -12,6 +13,8 @@ interface BSPBuilderGetFacesOptions {
     uvs?: boolean;
     /** @default true */
     displacements?: boolean;
+    /** @default true */
+    generateNormals?: boolean;
 }
 
 
@@ -38,7 +41,8 @@ export class BSPBuilder {
 
 
 
-    public async getFaces(options: BSPBuilderGetFacesOptions = {}): Promise<THREE.BufferGeometry[][]> {
+    public async getFace(index: number, options: BSPBuilderGetFacesOptions = {}): Promise<THREE.BufferGeometry> {
+        // TODO: Only load necessary lumps for the options provided.
         if(!this.vertices) this.vertices = await this.reader.getVertices();
         if(!this.edges) this.edges = await this.reader.getEdges();
         if(!this.surfedges) this.surfedges = await this.reader.getSurfEdges();
@@ -48,122 +52,141 @@ export class BSPBuilder {
         if(!this.dispInfos) this.dispInfos = await this.reader.getDispInfos();
         if(!this.dispVerts) this.dispVerts = await this.reader.getDispVerts();
 
+        const face = this.faces[index];
+
+        // Get face vertices
+        let verts = [];
+
+        for(let i = 0; i < face.numEdges; i++) {
+            const surfedge = this.surfedges[face.firstEdge + i];
+            const edge = this.edges[Math.abs(surfedge)];
+            const vertex = this.vertices[edge[surfedge < 0 ? 1 : 0]];
+
+            verts.push(vertex);
+        }
+
+        let indices: number[] = [];
+
+        if(face.dispInfo == -1 || !(options.displacements ?? true)) {
+            // Not displacement
+
+            // Generate indices
+            for(let i = 0; i < verts.length-2; i++) {
+                indices.push(i+2, i+1, 0);
+            }
+
+        } else {
+            // Compute displacement geometry
+
+            if(verts.length != 4) {
+                throw new Error('BSP Invalid displacement.');
+            }
+
+            const dispInfo = this.dispInfos[face.dispInfo];
+            const size = Math.pow(2, dispInfo.power);
+
+            // Generate vertices
+            const closest = verts.reduce((closest, vert) => {
+                if(closest === undefined) return vert;
+                return (vert.distanceTo(dispInfo.startPosition) < closest.distanceTo(dispInfo.startPosition)) ? vert : closest;
+            });
+            const orientation = verts.findIndex(vert => vert == closest);
+
+            const v0 = verts[(orientation + 0) % 4];
+            const v1 = verts[(orientation + 1) % 4];
+            const v2 = verts[(orientation + 2) % 4];
+            const v3 = verts[(orientation + 3) % 4];
+
+            verts = [];
+
+            for(let i = 0; i < size + 1; i++) {
+                for(let j = 0; j < size + 1; j++) {
+                    const u = i / size;
+                    const v = j / size;
+
+                    const dispVertIndex = i * (size + 1) + j;
+                    const dispVert = this.dispVerts[dispInfo.dispVertStart + dispVertIndex];
+
+                    const vertex = new THREE.Vector3().lerpVectors(
+                        new THREE.Vector3().lerpVectors(v0, v1, u),
+                        new THREE.Vector3().lerpVectors(v3, v2, u),
+                        v
+                    );
+
+                    vertex.add(new THREE.Vector3().copy(dispVert.vec).multiplyScalar(dispVert.dist));
+
+                    verts.push(vertex);
+
+                }
+            }
+
+            // Generate indices
+            for(let i = 0; i < size; i++) {
+                for(let j = 0; j < size; j++) {
+                    const i0 = i * (size+1) + j;
+                    const i1 = (i+1) * (size+1) + j;
+                    const i2 = (i+1) * (size+1) + j+1;
+                    const i3 = i * (size+1) + j+1;
+
+                    indices.push(
+                        i2, i1, i0,
+                        i3, i2, i0
+                    );
+                }
+            }
+
+        }
+
+        // Construct geometry
+        const texInfo = this.texInfos[face.texInfo];
+        const texData = this.texDatas[texInfo.texData];
+
+        const geom = new THREE.BufferGeometry();
+        geom.setAttribute('position', new THREE.Float32BufferAttribute(verts.map(vert => vert.toArray()).flat(), 3));
+        if(options.reflectivity ?? false) {
+            const reflectivity = texData.reflectivity;
+            geom.setAttribute('color', new THREE.Float32BufferAttribute(verts.map(() => reflectivity.toArray()).flat(), 3));
+        }
+        geom.setIndex(indices);
+
+        if(options.uvs ?? true) {
+            // Get face UVs
+            const s = new THREE.Vector3(texInfo.textureVecs[0][0], texInfo.textureVecs[0][1], texInfo.textureVecs[0][2]);
+            const t = new THREE.Vector3(texInfo.textureVecs[1][0], texInfo.textureVecs[1][1], texInfo.textureVecs[1][2]);
+            const xOffset = texInfo.textureVecs[0][3];
+            const yOffset = texInfo.textureVecs[1][3];
+
+            const uvs: THREE.Vector2[] = verts.map(vert => {
+                return new THREE.Vector2(
+                    (s.dot(vert) + xOffset) / texData.width,
+                    (t.dot(vert) + yOffset) / texData.height
+                );
+            });
+
+            geom.setAttribute('uv', new THREE.Float32BufferAttribute(uvs.map(uv => uv.toArray()).flat(), 2));
+        }
+
+        if(options.generateNormals ?? true) {
+            geom.computeVertexNormals();
+        }
+
+        return geom;
+
+    }
+
+    public async getFaces(options: BSPBuilderGetFacesOptions = {}): Promise<THREE.BufferGeometry[][]> {
+        if(!this.faces) this.faces = await this.reader.getFaces();
+        if(!this.texInfos) this.texInfos = await this.reader.getTexInfos();
+        if(!this.texDatas) this.texDatas = await this.reader.getTexDatas();
+
         let matFaces: THREE.BufferGeometry[][] = [];
 
-        for(const face of this.faces) {
+        for(let i = 0; i < this.faces.length; i++) {
+            const geom = await this.getFace(i, options);
 
-            // Get face reflectivity
+            const face = this.faces[i];
             const texInfo = this.texInfos[face.texInfo];
             const texData = this.texDatas[texInfo.texData];
-            const reflectivity = texData.reflectivity;
-
-            // Get face vertices
-            let verts = [];
-
-            for(let i = 0; i < face.numEdges; i++) {
-                const surfedge = this.surfedges[face.firstEdge + i];
-                const edge = this.edges[Math.abs(surfedge)];
-                const vertex = this.vertices[edge[surfedge < 0 ? 1 : 0]];
-
-                verts.push(vertex);
-            }
-
-            let indices: number[] = [];
-
-            if(face.dispInfo == -1 || !(options.displacements ?? true)) {
-                // Not displacement
-
-                // Generate indices
-                for(let i = 0; i < verts.length-2; i++) {
-                    indices.push(i+2, i+1, 0);
-                }
-
-            } else {
-                // Compute displacement geometry
-
-                if(verts.length != 4) {
-                    throw new Error('BSP Invalid displacement.');
-                }
-
-                const dispInfo = this.dispInfos[face.dispInfo];
-                const size = Math.pow(2, dispInfo.power);
-
-                // Generate vertices
-                const closest = verts.reduce((closest, vert) => {
-                    if(closest === undefined) return vert;
-                    return (vert.distanceTo(dispInfo.startPosition) < closest.distanceTo(dispInfo.startPosition)) ? vert : closest;
-                });
-                const orientation = verts.findIndex(vert => vert == closest);
-
-                const v0 = verts[(orientation + 0) % 4];
-                const v1 = verts[(orientation + 1) % 4];
-                const v2 = verts[(orientation + 2) % 4];
-                const v3 = verts[(orientation + 3) % 4];
-
-                verts = [];
-
-                for(let i = 0; i < size + 1; i++) {
-                    for(let j = 0; j < size + 1; j++) {
-                        const u = i / size;
-                        const v = j / size;
-
-                        const dispVertIndex = i * (size + 1) + j;
-                        const dispVert = this.dispVerts[dispInfo.dispVertStart + dispVertIndex];
-
-                        const vertex = new THREE.Vector3().lerpVectors(
-                            new THREE.Vector3().lerpVectors(v0, v1, u),
-                            new THREE.Vector3().lerpVectors(v3, v2, u),
-                            v
-                        );
-
-                        vertex.add(new THREE.Vector3().copy(dispVert.vec).multiplyScalar(dispVert.dist));
-
-                        verts.push(vertex);
-
-                    }
-                }
-
-                // Generate indices
-                for(let i = 0; i < size; i++) {
-                    for(let j = 0; j < size; j++) {
-                        const i0 = i * (size+1) + j;
-                        const i1 = (i+1) * (size+1) + j;
-                        const i2 = (i+1) * (size+1) + j+1;
-                        const i3 = i * (size+1) + j+1;
-
-                        indices.push(
-                            i2, i1, i0,
-                            i3, i2, i0
-                        );
-                    }
-                }
-
-            }
-
-            // Construct face
-            const geom = new THREE.BufferGeometry();
-            geom.setAttribute('position', new THREE.Float32BufferAttribute(verts.map(vert => vert.toArray()).flat(), 3));
-            if(options.reflectivity ?? false) {
-                geom.setAttribute('color', new THREE.Float32BufferAttribute(verts.map(() => reflectivity.toArray()).flat(), 3));
-            }
-            geom.setIndex(indices);
-
-            if(options.uvs ?? true) {
-                // Get face UVs
-                const s = new THREE.Vector3(texInfo.textureVecs[0][0], texInfo.textureVecs[0][1], texInfo.textureVecs[0][2]);
-                const t = new THREE.Vector3(texInfo.textureVecs[1][0], texInfo.textureVecs[1][1], texInfo.textureVecs[1][2]);
-                const xOffset = texInfo.textureVecs[0][3];
-                const yOffset = texInfo.textureVecs[1][3];
-
-                const uvs: THREE.Vector2[] = verts.map(vert => {
-                    return new THREE.Vector2(
-                        (s.dot(vert) + xOffset) / texData.width,
-                        (t.dot(vert) + yOffset) / texData.height
-                    );
-                });
-
-                geom.setAttribute('uv', new THREE.Float32BufferAttribute(uvs.map(uv => uv.toArray()).flat(), 2));
-            }
 
             // Add face to list
             if(!matFaces[texData.nameStringTableID]) {
@@ -215,17 +238,22 @@ export class BSPBuilder {
 
         for(const overlay of this.overlays) {
 
-            const size = new THREE.Vector3(100, 100, 100); // TODO - Get size of overlay
+            // Geometry to project onto.
+            let projectGeoms: THREE.BufferGeometry[] = [];
+            for(let i = 0; i < overlay.faceCount; i++) {
+                const faceIndex = overlay.faces[i];
+                const face = await this.getFace(faceIndex, { displacements: true, reflectivity: false, uvs: false, generateNormals: true });
+                projectGeoms.push(face);
+            }
 
+            // Project overlay.
+            const size = new THREE.Vector3(100, -100, 100); // TODO - Get size of overlay
+            const decal = new ThreeUtils.DecalGeometry(projectGeoms, overlay.origin, overlay.basisNormal, size);
 
-            // TODO - Use DecalGeometry to project onto overlay.faces
+            // Fix z-fighting.
+            // TODO - Use a different way to fix z-fighting.
+            decal.translate(overlay.basisNormal.x, overlay.basisNormal.y, overlay.basisNormal.z);
 
-            const decal = new THREE.PlaneGeometry(size.x, size.y); // TODO - Texture is mirrored horizontally
-            const up = new THREE.Vector3(0, 0, -1);
-            const right = new THREE.Vector3().crossVectors(up, overlay.basisNormal);
-            const projectionMatrix = new THREE.Matrix4().makeBasis(right, up, overlay.basisNormal);
-            projectionMatrix.setPosition(overlay.origin.clone().add(overlay.basisNormal)); // Add normal to stop z-fighting
-            decal.applyMatrix4(projectionMatrix);
 
 
             const texInfo = this.texInfos[overlay.texInfo];
